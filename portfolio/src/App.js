86d5eb6a9f7cbded23e6a0cba5c6d66878fdf967
BrDown <BrDown@gmail.com>
2024-10-23 18:43:35 -0400

    website static test

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <h1>ALEC STAR</h1>
+      <div className='content'>
+        {/* <img src="./IMG_5042.JPG" width='30vw'></img> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
